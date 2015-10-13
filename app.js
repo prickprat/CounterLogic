776dd5fd133106225644ba2d8a40bdb27d2c9052
prickprat <ricky.ratnayake@gmail.com>
2015-10-13 19:32:25 +1100

    experimenting with express

--- app.js
+++ app.js
@@ -1,16 +1,79 @@
+/*jshint strict:false */
+/*jslint node: true */
+
 var express = require('express');
 var app = express();
 
-var logger = require('./logger');
-app.use(logger);
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({ extended: false });
 
-app.use(express.static('public'));
+var categoryStore = {
+  'Lotopia': 'Rough and mountainous',
+  'Caspiana': 'Sky-top island',
+  'Indigo': 'Vibrant and thriving',
+  'Paradise': 'Lush, green plantation',
+  'Flotilla': 'Bustling urban oasis'
+};
 
-app.get('/blocks', function (request, response) {
-    var blocks = ['Test', 'Test1', 'Test3'];
-    response.json(blocks);
-})
+//Format the category name to be leading Uppercase
+function parseCategoryName(name) {
+    var parsedName = name[0].toUpperCase() + name.slice(1).toLowerCase();
+    return parsedName;
+}
 
-app.listen(3000, function () {
-    console.log('Listening of 3000 \n');
+//Adds a new category to the store
+function createCategory(name, description) {
+    categoryStore[name] = description;
+    return name;
+}
+
+var router = express.Router();
+
+router.route('/')
+    .get(function (req, res) {
+        var categoryNames = Object.keys(categoryStore);
+        response.status(200).json(categoryNames);
+    })
+
+    .post(parseUrlencoded, function (req, res) {
+        if (req.body.name.length > 0) {
+            var categoryName = createCategory(req.body.name, req.body.description);
+            res.status(201).json(categoryName);
+        } else {
+            res.status(400).json('Invalid Category Name');
+        }
+    });
+
+router.route('/:name')
+    .all(function (req, res, next) {
+        req.categoryName = parseCategoryName(req.params.name);
+        next();
+    })
+
+    .get(function (req, res) {
+        var categoryInfo = categoryStore[req.categoryName];
+        if (categoryInfo) {
+            res.status(200).json(categoryInfo);
+        } else {
+            res.status(404).json("Category not found");
+        }
+    })
+
+    .delete(function (req, res) {
+        if(categoryStore[req.categoryName]) {
+            delete categoryStore[req.categoryName];
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(404);
+        }
+    });
+
+/* Serve static files from public directory */
+app.use(express.static(__dirname + '/public'));
+
+app.use('/categories', router);
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log('Server listening on port %d\n', port);
 });
---
+++ public/client.js
@@ -0,0 +1,49 @@
+$(function(){
+
+    function appendToCategoryList(categories) {
+        var list = [];
+        var content;
+
+        $.each(categories, function(index, categoryName) {
+            content = '<a href="/categories/' + categoryName + '">' + categoryName + '</a>' +
+                        ' <a href="#" data-categoryName="' + categoryName + '">' +
+                        '<img class="deleteButton" src="delete.png"></a>';
+            list.push($('<li>', { html: content }));
+        })
+
+        $('.category-list').append(list);
+    }
+
+    // Request all categories from server
+    $.get('/categories', appendToCategoryList);
+
+    // Creating a new category
+    $('form').submit(function (event){
+        event.preventDefault();
+
+        var form = $(this);
+        var categoryData = form.serialize();
+
+        $.post('/categories', categoryData, function (categoryName){
+            appendToCategoryList([categoryName]);
+            form.trigger('reset');
+        });
+
+    // Deleting a block
+    $('.category-list').click('a[data-categoryName]', function (event){
+        var target = $(event.currentTarget);
+
+        if(!confirm('Are you sure you want to delete?')) {
+            return false;
+        }
+
+        //Remove the block from the server and block list.
+        $.ajax({
+                type: 'DELETE',
+                url: '/categories/' + target.data('categoryName')
+            })
+            .done(function (){
+                target.parents('li').remove();
+            });
+    });
+});
